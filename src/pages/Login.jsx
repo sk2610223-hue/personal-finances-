dfaabaca92ad2b379ab6ef1c7b4975a36a0877ca
sk2610223-hue <YOUR_GITHUB_EMAIL>
2026-06-15 09:32:53 +0530

    Initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (email === "" || password === "") {
+      alert("Please fill all fields");
+      return;
+    }
+
+    navigate("/dashboard");
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-box" onSubmit={handleLogin}>
+        <h2>Personal Finance Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
